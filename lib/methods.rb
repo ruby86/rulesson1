f91f20a28cd44a22bdf5d1edec569af0aa7af425
xays <astra@planet.ee>
2016-10-05 21:52:27 +0300

    DAY #2 - циклы, символы

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,33 +1,54 @@
-#methods
-def set_user_name(name)
-  result = "Здравствуйте, #{name}"
-  return result
-end
+# #methods
+# def set_user_name(name)
+#   result = "Здравствуйте, #{name}"
+#   return result
+# end
+#
+# puts set_user_name("гарольд")
+#
+#
+#
+#
+#
+# #array
+# array = [1,2,3,4,5]
+# array[1] = "one"
+# puts array[1]
+#
+# array_two = ["one", "two", 3]
+# puts array_two[1]
+#
+#
+#
+#
+# #hash
+# food_name = {
+#     'banana' => 'fruit',
+#     'cherry' => 'berry'
+# }
+# puts food_name['cherry']
+#
+# timer = Hash.new(0)
+# timer['count'] = +3
+# p timer['count']
 
-puts set_user_name("гарольд")
+#символы
+# def race(hash)
+#   color = {
+#       :black => "негры",
+#       :white => "белые",
+#       :red => "краснокожие"
+#   }
+#   return color[hash]
+# end
+#
+# puts "то что вы выбрали было: #{race(:white)}"
 
-
-
-
-
-#array
-array = [1,2,3,4,5]
-array[1] = "one"
-puts array[1]
-
-array_two = ["one", "two", 3]
-puts array_two[1]
-
-
-
-
-#hash
-food_name = {
-    'banana' => 'fruit',
-    'cherry' => 'berry'
-}
-puts food_name['cherry']
-
-timer = Hash.new(0)
-timer['count'] = +3
-p timer['count']
+# #цикл
+# count = 1
+# while(count < 100)
+# count+=1
+#   now = Time.now
+# puts now
+#   sleep(0.1)
+# end
